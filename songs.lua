7c40984ac0b249ae6af7ed16757976f9b1c457f2
DieManFamilyMan <163704503+DieManFamilyMan@users.noreply.github.com>
2026-04-27 17:11:22 -0400

    Create songs.lua

---
+++ songs.lua
@@ -0,0 +1,104 @@
+local songs = {
+    ["Flamewall.mp3"] = {
+        Name = "Flamewall",
+        By = "Camellia",
+        Icon = "rbxassetid://15674077019"
+    },
+    ["tidalwave.mp3"] = {
+        Name = "tidal wave",
+        By = "Monstercat",
+        Icon = "rbxassetid://81012126985317"
+    },
+    ["TheAbyss.mp3"] = {
+        Name = "Revolt From The Abyss",
+        By = "Noah",
+        Icon = "rbxassetid://6937742258"
+    },
+    ["VoidTrack2.mp3"] = {
+        Name = "VoidTrack2",
+        By = "idk",
+        Icon = "rbxassetid://8541985869"
+    },
+    ["opm.mp3"] = {
+        Name = "One Punch Man Theme",
+        By = "i forgot",
+        Icon = "rbxassetid://341699733"
+    },
+    ["Flow.mp3"] = {
+        Name = "Flow",
+        By = "Creo",
+        Icon = "rbxassetid://126874236293539"
+    },
+    ["Omega.mp3"] = {
+        Name = "OOPARTS",
+        By = "Camellia",
+        Icon = "rbxassetid://8246853051"
+    },
+    ["YAN.mp3"] = {
+        Name = "You're Gonna Need a Bigger Ukelele",
+        By = "guy named Chris",
+        Icon = "rbxassetid://84315723937730"
+    },
+    ["WindsOfFjord.mp3"] = {
+        Name = "Winds Of Fjords",
+        By = "minomus",
+        Icon = "rbxassetid://84315723937730"
+    },
+    ["RipNTear.mp3"] = {
+        Name = "Rip And Tear (Aeternus)",
+        By = "Mick Gordon",
+        Icon = "rbxassetid://106672985862293"
+    },
+    ["Thermodynamix.mp3"] = {
+        Name = "Thermodynamix (Acheron)",
+        By = "DJ-Nate",
+        Icon = "rbxassetid://71759405800988"
+    },
+    ["SR20DET.mp3"] = {
+        Name = "SR20DET/Andromeda",
+        By = "blksmith",
+        Icon = "rbxassetid://123919403834515"
+    },
+    ["SlowDown.mp3"] = {
+        Name = "Slow Down",
+        By = "Creo",
+        Icon = "rbxassetid://18872171323"
+    },
+    ["grief.mp3"] = {
+        Name = "Stalemate/Grief",
+        By = "KzX",
+        Icon = "rbxassetid://72082626362014"
+    },
+    ["firework.mp3"] = {
+        Name = "classical vip (firework)",
+        By = "rukkus",
+        Icon = "rbxassetid://89169026464218"
+    },
+    ["subsuming.mp3"] = {
+        Name = "subsuming vortex",
+        By = "uhh i forgot",
+        Icon = "rbxassetid://18872171323"
+    },
+    ["scatteredfaith.mp3"] = {
+        Name = "scattered faith",
+        By = "kurokotei",
+        Icon = "rbxassetid://124116589336010"
+    },
+    ["deimos.mp3"] = {
+        Name = "Deimos",
+        By = "Solkrieg",
+        Icon = "rbxassetid://18872171323"
+    },
+    ["defeated.mp3"] = {
+        Name = "When You Find Me",
+        By = "Plenka",
+        Icon = "rbxassetid://93197008758948"
+    },
+    ["sevvend.mp3"] = {
+        Name = "sevvend clubstep",
+        By = "idk bro",
+        Icon = "rbxassetid://85747063158167"
+    },
+}
+
+return songs
